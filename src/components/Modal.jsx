4c8af7b2d8200b1db9986e9f6b1281150f84150b
JaviMx <javimxmine@hotmail.com>
2022-09-06 14:43:56 -0500

    boton de cierre a modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,6 +8,7 @@ const Modal = () => {
   return (
     <div className="top-0 left-0 w-full h-screen bg-slate-900 bg-opacity-80 fixed flex justify-center items-center">
         <div className="container bg-slate-900 flex flex-col justify-center items-center text-white">
+            <p className="absolute top-10 right-10 text-4xl cursor-pointer" onClick={() => setModal(false)}>X</p>
             <h2 className="font-bold text-5xl mt-4 text-white">Editar Dato</h2>
             <Formulario/>
         </div>
